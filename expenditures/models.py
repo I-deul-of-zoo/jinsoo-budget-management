cfa36a69899adc2bccf679439d46193c9af7c70e
YuJinsoo <realaqua23@gmail.com>
2023-11-11 01:03:27 +0900

    :sparkles:Feat : Expenditure(지출테이블) 모델링 및 migrations

--- expenditures/models.py
+++ expenditures/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+from budgets.models import Category
+
+User = get_user_model()
+
+class Expenditure(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_DEFAULT, default=1)
+    payment = models.PositiveIntegerField(default=0)
+    appropriate_payment = models.PositiveIntegerField(default=5000)
+    memo = models.TextField(null=True, blank=True)
+    is_exept = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
