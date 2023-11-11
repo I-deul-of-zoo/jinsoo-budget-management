4d2be912c6d70ea816d6562254176840f555f098
YuJinsoo <realaqua23@gmail.com>
2023-11-11 23:44:20 +0900

    :sparkles:Feat : JWT세팅 완료 로그인,로그아웃 엔드포인트 및 시리얼라이저 개발

---
+++ accounts/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.contrib.auth import authenticate
+
+User = get_user_model()
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = User
+        fields = '__all__'
+        
+    def create(self, **validated_data):
+        user = User(
+            username=validated_data.get('username')
+        )
+        user.set_password(validated_data.get('password'))
+        user.save()
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+        
+    def validate(self, data):
+        username = data.get('username', None)
+        password = data.get('password', None)
+
+        if username is None:
+            raise serializers.ValidationError('An username address is required to log in.')
+        
+        if password is None:
+            raise serializers.ValidationError('A password is required to log in.')
+        
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            raise serializers.ValidationError('A user with this email and password was not found')
+        
+        return {'username': user.username, 'user': user}
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,60 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from accounts.serializers import RegisterSerializer, LoginSerializer
+
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny, ]
+    
+    def post(self, response):
+        serializer = RegisterSerializer(data=response.data)
+        
+        if serializer.is_valid():
+            user = serializer.create(**serializer.validated_data)
+            
+            refresh = RefreshToken.for_user(user)
+            
+            return Response({
+                "message": "register success!",
+                "userinfo": serializer.data,
+                "token":{
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                    }
+                }, status=status.HTTP_201_CREATED)
+            
+        else:
+            return Response(
+                {
+                    "error": "failed to regist",
+                    "errors": serializer.errors
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny, ]
+    # serializer_class = LoginSerializer
+    
+    def post(self, request, *args, **kwargs):
+        serializer = LoginSerializer(data=request.data)
+        # TODO - 이거는 왜 에러나는지 get_serializer 함수 공부 필요.
+        # serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data.get('user')
+
+        refresh = RefreshToken.for_user(user)
+        res = {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }
+
+        return Response(res, status=status.HTTP_200_OK)
