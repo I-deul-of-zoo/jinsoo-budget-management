23126b7f1233131a1f301ab86d1b1062e19d7e44
YuJinsoo <realaqua23@gmail.com>
2023-11-13 12:24:24 +0900

    :recycle:Refact : 개발문서 url에 맞추기 위해 엔드포인트 클래스 수정

--- budgets/urls.py
+++ budgets/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from budgets.views import CategoryListView
+from budgets.views import CategoryListView, BudgetsView
 
 app_name = "budgets"
 # base_url: api/budgets/
 
 urlpatterns =[
-    path("", CategoryListView.as_view())
+    # path("", CategoryListView.as_view()),
+    path("", BudgetsView.as_view()),
 ]
