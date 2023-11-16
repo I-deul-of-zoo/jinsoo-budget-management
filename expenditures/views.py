e048635ae7da94e986185445929fed1d6352b462
YuJinsoo <realaqua23@gmail.com>
2023-11-16 16:31:24 +0900

    :sparkles:Feat : 지출 목록 조회 API에 쿼리파라미터로 기간,금액,카테고리 구분 조회 및 카테고리별 소비 통계내역 데이터 생성 기능 추가

--- expenditures/views.py
+++ expenditures/views.py
@@ -1,3 +1,7 @@
+import math
+import copy
+from datetime import date, datetime, timedelta
+
 from django.shortcuts import render, get_object_or_404
 
 from rest_framework.views import APIView
@@ -9,6 +13,12 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from expenditures.models import Expenditure
 from expenditures.serializers import ExpenditureSerializer, ExpenditureCreateSerializer, ExpenditureUpdateSerializer
 
+from budgets.models import CATEGORIES
+
+STATIC_FORM = dict.fromkeys(CATEGORIES.keys(), {"count":0, "sum":0})
+DICT_FORM = {"count":0, "sum":0}
+
+
 class ExpenditureListCreateView(APIView):
     permission_classes = [IsAuthenticated, ]
     
@@ -18,10 +28,44 @@ class ExpenditureListCreateView(APIView):
         '''
         user = request.user
         queryset = Expenditure.objects.filter(user=user.pk)
+        
+        query_params = request.query_params
+        start = query_params.get("start", str(date.today() - timedelta(days=30)))
+        end = query_params.get("end", str(date.today()))
+        category = query_params.get("category", "all")
+        min_amount = query_params.get("min", 0)
+        max_amount = query_params.get("max", 9999999999) ## 예산 최대값을 어떻게 정할 수 있을까?
+        
+        start_date = datetime.strptime(start, '%Y-%m-%d')
+        end_date =  datetime.strptime(end, '%Y-%m-%d')
+        end_date = end_date + timedelta(days=1)
+        
+        queryset = queryset.filter(created_at__gte=start_date, created_at__lt=end_date, amount__gte=min_amount, amount__lt=max_amount)
+        
+        if category != "all":
+            queryset = queryset.filter(category=category)
+        
         serializer = ExpenditureSerializer(queryset, many=True)
         result = serializer.data
+        
+        ## 리스트 통계 데이터 생성
+        statistics = dict()
+        temp_list = list()
+        for keyname in CATEGORIES.keys():
+            temp_list.append({keyname: copy.deepcopy(DICT_FORM)})
+            
+        statistics["category_group"] = temp_list
+        total = 0
+        for q in list(queryset):
+            total += q.amount
+            for d in statistics["category_group"]:
+                if d.get(q.category.name, None):
+                    d[q.category.name]["count"] += 1
+                    d[q.category.name]["sum"] += q.amount
+        
+        statistics["total_sum"] = total
 
-        return Response({"message": "sucess!", "data": result}, status=status.HTTP_200_OK)
+        return Response({"message": "success!", "data": result, "static_data":statistics}, status=status.HTTP_200_OK)
     
     def post(self, request):
         '''
@@ -29,7 +73,6 @@ class ExpenditureListCreateView(APIView):
         
         {
             "category" : 4,
-            "
         }
         '''
         user = request.user
